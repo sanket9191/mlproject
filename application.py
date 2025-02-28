fec54d0e04028c932bd8142bafd7b40089d6881e
sanket9191 <diwatesanket@gmail.com>
2025-02-28 20:58:10 +0530

    minor fix

--- application.py
+++ application.py
@@ -42,5 +42,5 @@ def predict_datapoint():
     
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 4000))  # 0 lets the OS assign a free port
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
    
